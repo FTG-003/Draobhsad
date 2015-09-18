ae10838aac310f444c9a815d362468fc5403d0f7
Fabrizio <siar@member.fsf.org>
2015-09-18 17:22:44 +0200

    git

---
+++ training/MIT-Apps/aia/src/appinventor/ai_fabrizio_terzi/PeeragogyApp/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"130","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"16","AlignHorizontal":"3","AlignVertical":"2","AppName":"PeeragogyApp","Title":"Screen1","Uuid":"0"}}
+|#
